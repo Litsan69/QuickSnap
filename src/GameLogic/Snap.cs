c2ade7bf85fad129911ea03f9fd7c46af01d0693
Litsan69 <mutasimaffanz3.0@gmail.com>
2025-04-28 22:09:34 +0600

    Update Snap.cs

--- src/GameLogic/Snap.cs
+++ src/GameLogic/Snap.cs
@@ -37,6 +37,7 @@ namespace CardGames.GameLogic
 		public Snap ()
 		{
 			_deck = new Deck ();
+   			_gameTimer = SwinGame.CreateTimer ();
 		}
 
 		/// <summary>
@@ -91,7 +92,8 @@ namespace CardGames.GameLogic
 				_started = true;
 				_deck.Shuffle ();		// Return the cards and shuffle
 
-				FlipNextCard ();		// Flip the first card...
+				FlipNextCard ();
+    				 gameTimer.Start ();
 			}
 		}
 			
@@ -111,7 +113,11 @@ namespace CardGames.GameLogic
 		/// </summary>
 		public void Update()
 		{
-			//TODO: implement update to automatically slip cards!
+			if (_gameTimer.Ticks > _flipTime)
+   			{
+      				_gameTimer.Reset ();
+	  			FlipNextCard ();
+			}//TODO: implement update to automatically slip cards!
 		}
 
 		/// <summary>
